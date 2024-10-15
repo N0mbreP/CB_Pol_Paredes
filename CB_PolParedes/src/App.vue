f5b70dce9ddd7f17a1acf4bed23c9f7b673b7298
N0mbreP <pol.paredes.7e7@itb.cat>
2024-10-15 19:45:02 +0200

    Comit 2

--- CB_PolParedes/src/App.vue
+++ CB_PolParedes/src/App.vue
@@ -14,6 +14,43 @@ const usuari=reactive({
 const augmentarEdat=()=>{
   usuari.edat++
 }
+
+//Exemple directiva v-bind
+const titol="hola"
+const imatgeUrl= ref("https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/653.png")
+const canviar = ()=>{
+  imatgeUrl.value= "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/700.png"
+}
+
+//Exemple directiva v-mode
+const nomUsuari=ref('')
+
+//Exemple directiva v-for
+const productes=[
+  {
+    id:1,
+    nom:"Pila",
+    preu:6
+  },
+  {
+    id:2,
+    nom:"Poma",
+    preu:8
+  },
+  {
+    id:3,
+    nom:"Aigua",
+    preu:31
+  }
+]
+
+// Exemple directiva v-if v-else
+const logat=ref(false)
+
+//Exemple directiva v-show
+const mostrar=ref(false)
+
+//Directiva v-on = @
 </script>
 
 <template>
@@ -22,10 +59,43 @@ const augmentarEdat=()=>{
     <h3>{{ num }}</h3>
     <button @click="incrementar">Incrementar</button>
   </div>
+  
   <div>
     <h1>Exemple reactive</h1>
     <p>{{ usuari.nom }}</p>
     <p>{{ usuari.edat }}</p>
+    <button @click="augmentarEdat">Augmentar edat</button>
+  </div>
+  
+  <div>
+    <h1>Exemple V-bind</h1>
+    <h2 v-bind:title="titol">Possa el ratoli per sobre</h2>
+    <img  :src="imatgeUrl"  alt="imagen" @click="canviar">
+  </div>
+
+  <div>
+    <h1>Exemple v-mode</h1>
+    <p>Nom usuari: {{ nomUsuari }}</p>
+    <input type="text" v-model="nomUsuari" placeholder="Escriu aqui el teu nom">
+  </div>
+  
+  <div>
+    <h1>Exemple v-for</h1>
+    <ul>
+      <li v-for="producte in productes" :key="producte.id">{{ producte.nom }}  -  {{ producte.preu }} €</li>
+    </ul>
+  </div>
+
+  <div>
+    <h1>Exemle v-if i v-else</h1>
+    <p v-if="logat">Hola!!! ^-^</p>
+    <p v-else="logat">Inicia sessio porfa n-n</p>
+  </div>
+
+  <div>
+    <h1>Exemple v-show</h1>
+    <button @click="mostrar=!mostrar">Mostrar</button>
+    <p v-show="mostrar">n-n</p>
   </div>
 </template>
 
